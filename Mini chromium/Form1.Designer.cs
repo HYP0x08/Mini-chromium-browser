25a9c4c2ffaadd003dc58e90bfef547e71a5346c
Tongke <Sagirisan@qq.vip.com>
2017-11-26 19:54:47 +0800

    Hello Mini-chromium-browser, you're like a freshman now

---
+++ Mini chromium/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 396);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作区域";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(549, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开发者工具";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "要执行的JS命令：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(653, 50);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "转跳区域";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(573, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "转跳";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(501, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "URL地址：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(659, 447);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Mini chromium/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Chromium;
+using Chromium.Remote.Event;
+using Chromium.WebBrowser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            ChromiumWebBrowser.Initialize();
+            InitializeComponent();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CfxRuntime.Shutdown();
+        }
+        public void CfxHelloWorld_Execute(object sender, CfrV8HandlerExecuteEventArgs e)
+        {
+            MessageBox.Show("调用C#方法成功");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(wb.ExecuteJavascript(textBox1.Text));
+        }
+        ChromiumWebBrowser wb = new ChromiumWebBrowser();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser cb = new ChromiumWebBrowser();
+            wb = cb;
+            wb.Location = new Point(0,50);
+            wb.Width = Width;
+            wb.Height = Height - 120;
+            wb.GlobalObject.AddFunction("s_domain").Execute += new CfrV8HandlerExecuteEventHandler(CfxHelloWorld_Execute);
+            Controls.Add(wb);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CfxWindowInfo windowInfo = new CfxWindowInfo();
+                windowInfo.Style = WindowStyle.WS_OVERLAPPEDWINDOW | WindowStyle.WS_CLIPCHILDREN | WindowStyle.WS_CLIPSIBLINGS | WindowStyle.WS_VISIBLE;
+                windowInfo.ParentWindow = IntPtr.Zero;
+                windowInfo.WindowName = "开发人员工具";
+                windowInfo.X = 200;
+                windowInfo.Y = 200;
+                windowInfo.Width = 800;
+                windowInfo.Height = 600;
+                wb.Browser.Host.ShowDevTools(windowInfo, new CfxClient(), new CfxBrowserSettings(), null);
+            }
+            catch
+            {
+                Console.WriteLine("请等待页面加载完成之后再打开调试器");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wb.LoadUrl(textBox2.Text);
+        }
+    }
+}
